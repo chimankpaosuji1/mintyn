f04961ab72e0bd7e29bee86ba01fc994bdd7e670
chimankpaosuji1 <57535728+chimankpaosuji1@users.noreply.github.com>
2024-01-26 17:49:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { DashboardPage } from "./routes/Routes.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import "./styles/index.scss"
 
 const App = () => {
   return (
